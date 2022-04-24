d56163af89a95b1696229beb1ad68cb4b42cd8b2
Tibor Leupold <tibor@lpld.io>
2022-04-24 11:30:22 -0700

    Create custom image model

---
+++ lpld/images/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from wagtail.images import models as image_models
+
+
+class CustomImage(image_models.AbstractImage):
+
+    admin_form_fields = image_models.Image.admin_form_fields
+
+
+class CustomRendition(image_models.AbstractRendition):
+    image = models.ForeignKey(
+        "images.CustomImage",
+        on_delete=models.CASCADE,
+        related_name="renditions",
+    )
+
+    class Meta:
+        unique_together = (
+            ("image", "filter_spec", "focal_point_key"),
+        )
