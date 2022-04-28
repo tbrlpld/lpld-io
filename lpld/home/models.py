fd3b032a52fc131e2f0b6c605b285b70a0eb3bcd
Tibor Leupold <tibor@lpld.io>
2022-04-27 18:27:23 -0700

    Add introduction field to home page

--- lpld/home/models.py
+++ lpld/home/models.py
@@ -1,5 +1,7 @@
 from django.apps import apps
 
+from wagtail.admin import edit_handlers
+from wagtail.core import fields
 from wagtail.core import models as wagtail_models
 
 
@@ -8,6 +10,12 @@ class HomePage(wagtail_models.Page):
 
     max_count = 1
     template = "pages/home/home.html"
+
+    introduction = fields.RichTextField(features=["link"], null=True, blank=True)
+
+    content_panels = wagtail_models.Page.content_panels + [
+        edit_handlers.FieldPanel("introduction"),
+    ]
 
     def get_context(self, request):
         context = super().get_context(request)
