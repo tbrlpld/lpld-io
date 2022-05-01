f07046daf2d641e4538718b0b527365e4c7f907e
Tibor Leupold <tibor@lpld.io>
2022-05-01 14:57:22 -0700

    Add profile image field to home

--- lpld/home/models.py
+++ lpld/home/models.py
@@ -1,8 +1,11 @@
 from django.apps import apps
+from django.db import models
 
+from wagtail import images as wagtail_images
 from wagtail.admin import edit_handlers
 from wagtail.core import fields
 from wagtail.core import models as wagtail_models
+from wagtail.images import edit_handlers as image_panels
 
 
 class HomePage(wagtail_models.Page):
@@ -12,9 +15,16 @@ class HomePage(wagtail_models.Page):
     template = "pages/home/home.html"
 
     introduction = fields.RichTextField(features=["link"], null=True, blank=True)
+    profile_image = models.ForeignKey(
+        wagtail_images.get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
 
     content_panels = wagtail_models.Page.content_panels + [
         edit_handlers.FieldPanel("introduction"),
+        image_panels.ImageChooserPanel("profile_image"),
     ]
 
     def get_context(self, request):
