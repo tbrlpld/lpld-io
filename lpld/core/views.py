9b8c852dd512fc74621dd519c69626f36711a615
Tibor Leupold <tibor@lpld.io>
2022-05-30 17:36:46 -0700

    Add 400 error template and view

--- lpld/core/views.py
+++ lpld/core/views.py
@@ -6,13 +6,17 @@ class RobotsView(generic_views.TemplateView):
     template_name = "pages/robots.txt"
 
 
+def handle_400(request, exception):
+    return shortcuts.render(request, template_name="pages/errors/400.html", status=400)
+
+
 def handle_404(request, exception):
     return shortcuts.render(request, template_name="pages/errors/404.html", status=404)
 
 
 def error_test_view(request, error_code):
     error_views = {
+        400: handle_400,
         404: handle_404,
     }
     return error_views[error_code](request, exception=None)
-
