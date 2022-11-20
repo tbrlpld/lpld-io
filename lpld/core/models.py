1d4ae9ab7738c26940d00b46c0ccf5ebd3b0ea1c
Tibor Leupold <tibor@lpld.io>
2022-11-19 16:04:52 -0800

    Move title tag to base page model
    It's just way cleaner to keep this stuff in the base page than in the template.
    It also provides an easy way to override this on the other page types.

--- lpld/core/models.py
+++ lpld/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import functional
+from django.utils import functional as func_utils
 
 from wagtail.core import models as wagtail_models
 from wagtail.admin import panels
@@ -10,7 +10,16 @@ class BasePage(wagtail_models.Page):
     class Meta(wagtail_models.Page.Meta):
         abstract = True
 
-    @functional.cached_property
+    @func_utils.cached_property
+    def title_tag_content(self):
+        return self.get_title_tag_content()
+
+    def get_title_tag_content(self):
+        title_text = self.seo_title or self.title
+        title_text = f"{ title_text } · lpld.io"
+        return title_text
+
+    @func_utils.cached_property
     def meta_description(self):
         return self.get_meta_description
 
