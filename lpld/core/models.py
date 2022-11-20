51eb0d98f9bc8d3e6f0a6615d109832323ad378d
Tibor Leupold <tibor@lpld.io>
2022-11-19 17:32:34 -0800

    Add TODO comment

--- lpld/core/models.py
+++ lpld/core/models.py
@@ -26,6 +26,7 @@ class BasePage(wagtail_models.Page):
         return self.search_description or ""
 
 
+# TODO: Once the technologies.Technology model is in production, this model can be deleted.
 class Technology(models.Model):
     name = models.CharField(max_length=250, null=False, blank=False)
 
