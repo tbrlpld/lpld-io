3865b7e1fdb92cd02de9f4974340a788a8607e9d
Tibor Leupold <tibor@lpld.io>
2022-11-20 10:57:44 -0800

    Delete model core.Technology

--- lpld/core/models.py
+++ lpld/core/models.py
@@ -24,19 +24,3 @@ class BasePage(wagtail_models.Page):
 
     def get_meta_description(self):
         return self.search_description or ""
-
-
-# TODO: Once the technologies.Technology model is in production,
-#       this model can be deleted.
-class Technology(models.Model):
-    name = models.CharField(max_length=250, null=False, blank=False)
-
-    panels = [
-        panels.FieldPanel("name"),
-    ]
-
-    class Meta:
-        verbose_name_plural = "Technologies"
-
-    def __str__(self):
-        return self.name
