bd7b5fe0c1acfc3cb024a684fbab820a7c5874e5
Tibor Leupold <tibor@lpld.io>
2023-01-11 16:01:04 -0800

    Add minimal test for homepage

---
+++ tests/test_home/test_models.py
@@ -0,0 +1,15 @@
+import http
+
+import pytest
+
+from lpld.home import factories
+
+
+@pytest.mark.django_db
+class TestHomePage:
+    def test_page_loads(self, client):
+        home_page = factories.HomePage()
+
+        response = client.get(path=home_page.get_url())
+
+        assert response.status_code == http.HTTPStatus.OK
