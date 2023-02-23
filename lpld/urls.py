de39e7dd3f28342438315fae4bfe0a5d8828aa95
Tibor Leupold <tibor@lpld.io>
2023-02-22 17:04:32 -0800

    Use module level import in urls

--- lpld/urls.py
+++ lpld/urls.py
@@ -5,17 +5,17 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    2. Add a URL to urlpatterns:  urls.path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  urls.path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the urls.include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  urls.path('blog/', urls.include('blog.urls'))
 """
+from django import urls
 from django.conf import settings
 from django.conf.urls import static as static_urls
-from django.urls import include, path
 
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtail_admin_urls
@@ -25,10 +25,10 @@ from wagtail.documents import urls as wagtail_docs_urls
 from lpld.core import views as core_views
 
 urlpatterns = [
-    path("docs/", include(wagtail_docs_urls)),
-    path("lpld-admin/", include(wagtail_admin_urls)),
-    path("robots.txt", core_views.RobotsView.as_view()),
-    path("sitemap.xml", sitemap_views.sitemap),
+    urls.path("docs/", urls.include(wagtail_docs_urls)),
+    urls.path("lpld-admin/", urls.include(wagtail_admin_urls)),
+    urls.path("robots.txt", core_views.RobotsView.as_view()),
+    urls.path("sitemap.xml", sitemap_views.sitemap),
 ]
 
 
@@ -36,15 +36,15 @@ if "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar  # type: ignore
 
     urlpatterns.append(
-        path("__debug__/", include(debug_toolbar.urls)),
+        urls.path("__debug__/", urls.include(debug_toolbar.urls)),
     )
 
 
 if settings.DEBUG:
     urlpatterns.extend(
         [
-            path("pattern-library/", include("pattern_library.urls")),
-            path("-/error/<int:error_code>/", core_views.error_test_view),
+            urls.path("pattern-library/", urls.include("pattern_library.urls")),
+            urls.path("-/error/<int:error_code>/", core_views.error_test_view),
         ]
     )
     urlpatterns.extend(
@@ -59,10 +59,10 @@ if settings.SENTRY_TEST:
     def trigger_error(request):
         1 / 0
 
-    urlpatterns.append(path("-/sentry-test/", trigger_error))  # type: ignore
+    urlpatterns.append(urls.path("-/sentry-test/", trigger_error))  # type: ignore
 
 
-urlpatterns.append(path("", include(wagtail_urls)))
+urlpatterns.append(urls.path("", urls.include(wagtail_urls)))
 
 
 handler400 = core_views.handle_400
