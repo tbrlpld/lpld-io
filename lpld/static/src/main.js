cf58aff347b6815caeda1dee0a366d0dfcd44318
Tibor Leupold <tibor@lpld.io>
2023-02-24 19:17:52 -0800

    Pass sentry settings to JS

--- lpld/static/src/main.js
+++ lpld/static/src/main.js
@@ -1,1 +1,16 @@
 // Main entry point
+
+
+function main(){
+    const sentrySettings = getSentrySettings()
+}
+
+
+function getSentrySettings() {
+    const sentrySettingsElement = document.querySelector('#sentry-settings')
+    const sentrySettings = JSON.parse(sentrySettingsElement.textContent)
+    return sentrySettings
+}
+
+
+(function(){main()})()
