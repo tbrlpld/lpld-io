80e33c02d61cd3cf64239ccd11d9950fbb06769c
Tibor Leupold <tibor@lpld.io>
2023-02-24 19:39:19 -0800

    Fix js to standard style

--- lpld/static/src/main.js
+++ lpld/static/src/main.js
@@ -1,33 +1,30 @@
 // Main entry point
-import * as Sentry from "@sentry/browser"
-import { BrowserTracing } from "@sentry/tracing"
+import * as Sentry from '@sentry/browser'
+import { BrowserTracing } from '@sentry/tracing'
 
-
-function main(){
-    setUpSentry()
+function main () {
+  setUpSentry()
 }
 
-
-function setUpSentry() {
-    const sentrySettings = getSentrySettings()
-    Sentry.init({
-        dsn: sentrySettings.SENTRY_DSN,
-        integrations: [new BrowserTracing()],
-        tracesSampleRate: sentrySettings.SENTRY_SAMPLE_RATE,
-        environment: sentrySettings.SENTRY_ENVIRONMENT,
-        release: sentrySettings.HEROKU_RELEASE_VERSION,
-    })
-    if (sentrySettings.SENTRY_TEST) {
-        nonExistentFunction()
-    }
+function setUpSentry () {
+  const sentrySettings = getSentrySettings()
+  Sentry.init({
+    dsn: sentrySettings.SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: sentrySettings.SENTRY_SAMPLE_RATE,
+    environment: sentrySettings.SENTRY_ENVIRONMENT,
+    release: sentrySettings.HEROKU_RELEASE_VERSION
+  })
+  if (sentrySettings.SENTRY_TEST) {
+    // This is calling a non existing function to test the error monitoring.
+    nonExistentFunction() // eslint-disable-line no-undef
+  }
 }
 
-
-function getSentrySettings() {
-    const sentrySettingsElement = document.querySelector('#sentry-settings')
-    const sentrySettings = JSON.parse(sentrySettingsElement.textContent)
-    return sentrySettings
+function getSentrySettings () {
+  const sentrySettingsElement = document.querySelector('#sentry-settings')
+  const sentrySettings = JSON.parse(sentrySettingsElement.textContent)
+  return sentrySettings
 }
 
-
-(function(){main()})()
+(function () { main() })()
