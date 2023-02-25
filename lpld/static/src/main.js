c663d5a75a8c1f65461634c596938a0e3339410e
Tibor Leupold <tibor@lpld.io>
2023-02-24 19:35:25 -0800

    Move sentry setup to function

--- lpld/static/src/main.js
+++ lpld/static/src/main.js
@@ -1,8 +1,25 @@
 // Main entry point
+import * as Sentry from "@sentry/browser"
+import { BrowserTracing } from "@sentry/tracing"
 
 
 function main(){
+    setUpSentry()
+}
+
+
+function setUpSentry() {
     const sentrySettings = getSentrySettings()
+    Sentry.init({
+        dsn: sentrySettings.SENTRY_DSN,
+        integrations: [new BrowserTracing()],
+        tracesSampleRate: sentrySettings.SENTRY_SAMPLE_RATE,
+        environment: sentrySettings.SENTRY_ENVIRONMENT,
+        release: sentrySettings.HEROKU_RELEASE_VERSION,
+    })
+    if (sentrySettings.SENTRY_TEST) {
+        nonExistentFunction()
+    }
 }
 
 
