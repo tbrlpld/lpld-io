45a64919c7b98c056411af517217ba4325c645d1
Tibor Leupold <tibor@lpld.io>
2023-02-24 18:41:45 -0800

    Add context processor for sentry settings

---
+++ lpld/core/context_processors.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+
+def sentry_settings(request):
+    return {
+        "SENTRY_DSN": settings.SENTRY_DSN,
+        "SENTRY_SAMPLE_RATE": settings.SENTRY_SAMPLE_RATE,
+        "SENTRY_ENVIRONMENT": settings.SENTRY_ENVIRONMENT,
+        "HEROKU_RELEASE_VERSION": settings.HEROKU_RELEASE_VERSION,
+    }
---
+++ tests/test_core/test_context_processors.py
@@ -0,0 +1,22 @@
+from lpld.core import context_processors
+
+
+def test_sentry_settings(settings, rf):
+    SENTRY_DSN: str = "https://example.com"
+    SENTRY_SAMPLE_RATE: float = 1.0
+    SENTRY_ENVIRONMENT: str = "test"
+    HEROKU_RELEASE_VERSION: str = "1"
+    settings.SENTRY_DSN = SENTRY_DSN
+    settings.SENTRY_SAMPLE_RATE = SENTRY_SAMPLE_RATE
+    settings.SENTRY_ENVIRONMENT = SENTRY_ENVIRONMENT
+    settings.HEROKU_RELEASE_VERSION = HEROKU_RELEASE_VERSION
+    request = rf.get("/")
+
+    context = context_processors.sentry_settings(request)
+
+    assert context == {
+        "SENTRY_DSN": SENTRY_DSN,
+        "SENTRY_SAMPLE_RATE": SENTRY_SAMPLE_RATE,
+        "SENTRY_ENVIRONMENT": SENTRY_ENVIRONMENT,
+        "HEROKU_RELEASE_VERSION": HEROKU_RELEASE_VERSION,
+    }
