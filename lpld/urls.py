d228bc36b36b7a6c76fe5c9b804a2f4f2aaa6d82
Tibor Leupold <tibor@lpld.io>
2023-02-25 19:44:41 -0800

    Use proxy to serve plausible script

--- lpld/urls.py
+++ lpld/urls.py
@@ -68,7 +68,10 @@ if settings.SENTRY_TEST:
     urlpatterns.append(urls.path("-/sentry-test/", trigger_error))  # type: ignore
 
 
-urlpatterns.append(urls.path("", urls.include(wagtail_urls)))
+urlpatterns.extend([
+    urls.path("", urls.include(wagtail_urls)),
+    urls.path("", urls.include("plausible_proxy.urls")),
+])
 
 
 handler400 = core_views.handle_400
