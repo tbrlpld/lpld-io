52514f3e898c78489764e0a44449582aec248f6c
Tibor Leupold <tibor@lpld.io>
2023-03-02 19:06:08 -0800

    Create sentry meta templatetag

--- lpld/templatetags/lpldutils.py
+++ lpld/templatetags/lpldutils.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils import safestring
+
+import sentry_sdk
 
 register = template.Library()
 
@@ -6,3 +9,8 @@ register = template.Library()
 @register.filter
 def split(value, splitter=" "):
     return value.split(splitter)
+
+
+@register.simple_tag()
+def sentry_meta():
+    return safestring.mark_safe(sentry_sdk.Hub.current.trace_propagation_meta())
