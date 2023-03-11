e42094eb39640777301381c10d02184903769cca
Tibor Leupold <tibor@lpld.io>
2023-03-11 15:52:39 -0800

    Pull db in release phase and re-run release in run phase

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+echo "Restoring the SQLite database from bucket."
+litestream restore -config litestream.yml -if-db-not-exists -if-replica-exists "$DB_DIR/db.sqlite3"
+
 ./manage.py check --deploy --fail-level WARNING
 ./manage.py createcachetable
 ./manage.py migrate --noinput
