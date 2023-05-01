611893f0b740c272f69e0a215a1590283c433d23
Tibor Leupold <tibor@lpld.io>
2023-04-30 18:07:05 -0700

    Test and implement factory setting required fields

--- lpld/core/factories/models.py
+++ lpld/core/factories/models.py
@@ -4,8 +4,16 @@ import wagtail_factories
 from lpld.core import models as core_models
 
 
+class PrimaryNavigationSettingFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = core_models.PrimaryNavigationSetting
+
+    site = factory.SubFactory(wagtail_factories.SiteFactory)
+
+
 class PrimaryNavigationLinkFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = core_models.PrimaryNavigationLink
 
+    primary_navigation = factory.SubFactory(PrimaryNavigationSettingFactory)
     page = factory.SubFactory(wagtail_factories.PageFactory)
--- tests/test_core/test_factories/test_models.py
+++ tests/test_core/test_factories/test_models.py
@@ -1,8 +1,17 @@
 import pytest
 from wagtail import models as wagtail_models
 
-
+from lpld.core import models as core_models
 from lpld.core.factories import models as model_factories
+
+
+@pytest.mark.django_db
+class TestPrimaryNavigationSettingFactory:
+    def test_factory(self):
+        setting = model_factories.PrimaryNavigationSettingFactory()
+
+        assert setting.site
+        assert isinstance(setting.site, wagtail_models.Site)
 
 
 @pytest.mark.django_db
@@ -12,3 +21,10 @@ class TestNavigationLinkFactory:
 
         assert link.page
         assert isinstance(link.page, wagtail_models.Page)
+        assert link.primary_navigation
+        assert isinstance(link.primary_navigation, core_models.PrimaryNavigationSetting)
+
+    def test_can_set_text_override(self):
+        link = model_factories.PrimaryNavigationLinkFactory(text_override="foo")
+
+        assert link.text_override == "foo"
