43a4e94fefead810a6d68530418b623c37ed511f
Tibor Leupold <tibor@lpld.io>
2023-09-09 17:31:02 -0700

    Update test to recent changes

--- tests/test_home/test_models.py
+++ tests/test_home/test_models.py
@@ -30,14 +30,14 @@ class TestHomePage:
             introduction="Introduction 2",
         )
 
-        teasers = home_page.project_teasers
+        teaser_grid = home_page.project_teasers
 
-        assert len(teasers) == 2
-        teaser_1 = teasers[0]
+        assert len(teaser_grid.teasers) == 2
+        teaser_1 = teaser_grid.teasers[0]
         assert teaser_1.heading == project_page_1.title
         assert teaser_1.introduction == project_page_1.introduction
         assert teaser_1.href == project_page_1.get_url()
-        teaser_2 = teasers[1]
+        teaser_2 = teaser_grid.teasers[1]
         assert teaser_2.heading == project_page_2.title
         assert teaser_2.introduction == project_page_2.introduction
         assert teaser_2.href == project_page_2.get_url()
