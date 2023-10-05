c8482cecdbe1842c5633d28b043a9995a88a85db
Tibor Leupold <tibor@lpld.io>
2023-10-04 17:58:48 -0700

    Add type hints to decorator
    It does not really help... the dataclass decorator needs to be added to the specific
    class to make mypy understand the arguments.
    
    See also:
    https://mypy.readthedocs.io/en/stable/additional_features.html#caveats-known-issues

--- lpld/templex/__init__.py
+++ lpld/templex/__init__.py
@@ -1,11 +1,11 @@
 import dataclasses
 import functools
-from typing import Type
+from typing import Callable, Protocol, Type, cast
 
-from django import template as django_template
+from django.template import loader
 
 
-def templex(template: str):
+def templex(template: str) -> Callable:
     """
     Turn class into a templex object.
 
@@ -15,7 +15,7 @@ def templex(template: str):
 
     """
 
-    def wrapper(klass):
+    def wrapper(klass: Type) -> TemplexProtocol:
         """
         The actual decorator.
 
@@ -23,14 +23,14 @@ def templex(template: str):
         surrounding scope.
 
         """
-        def render_templex(self, **kwargs):
+        def render_templex(self, **kwargs: dict) -> str:
             """
             Render the templex by passing its data into the template.
 
             The context can be extended by passing keyword arguments to this method.
 
             """
-            templex_template = django_template.loader.get_template(self.template)
+            templex_template = loader.get_template(self.template)
             # Shallow copy to avoid the resolution of nested templexes. We need the
             # nested templexes to stay templexes so that they can be rendered later.
             data_dict = dict(
@@ -42,17 +42,26 @@ def templex(template: str):
             return templex_template.render(data_dict)
 
         @functools.wraps(klass)
-        def wrap(klass: Type) -> Type:
+        def wrap(klass: Type) -> TemplexProtocol:
             # Turn class into a dataclass.
-            klass = dataclasses.dataclass(klass)
+            dataklass = dataclasses.dataclass(klass)
             # Add the template property to the class if it doesn't already exist.
-            if not hasattr(klass, "template"):
-                klass.template = template
+            if not hasattr(dataklass, "template"):
+                dataklass.template = template
             # Add the render method to the class if it doesn't already exist.
-            if not hasattr(klass, "render"):
-                klass.render = render_templex
-            return klass
+            if not hasattr(dataklass, "render"):
+                dataklass.render = render_templex
+            return cast(TemplexProtocol, dataklass)
 
         return wrap(klass)
 
     return wrapper
+
+
+@dataclasses.dataclass
+class DataclassProtocol(Protocol):
+    pass
+
+class TemplexProtocol(DataclassProtocol):
+    template: str
+    render: Callable
