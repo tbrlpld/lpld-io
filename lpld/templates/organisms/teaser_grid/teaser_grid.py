bb5e4a8bc16ce91c33489e7ab79e04d64bb34a45
Tibor Leupold <tibor@lpld.io>
2023-10-07 16:46:03 -0700

    Use immutable

--- lpld/templates/organisms/teaser_grid/teaser_grid.py
+++ lpld/templates/organisms/teaser_grid/teaser_grid.py
@@ -4,7 +4,7 @@ from lpld.templates.molecules.teaser import teaser
 from lpld import templex
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class TeaserGrid(templex.Templex):
     template="organisms/teaser_grid/teaser-grid.html"
     teasers: list[teaser.Teaser]
