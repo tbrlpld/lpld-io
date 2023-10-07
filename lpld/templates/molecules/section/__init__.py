c6f9f24f2e4cd94810a94a73cc5b8e6f7392b782
Tibor Leupold <tibor@lpld.io>
2023-10-07 16:45:54 -0700

    Fix format

--- lpld/templates/molecules/section/__init__.py
+++ lpld/templates/molecules/section/__init__.py
@@ -6,6 +6,7 @@ from lpld import templex
 @dataclasses.dataclass(frozen=True)
 class Section(templex.Templex):
     template="molecules/section/section.html"
+
     content: list[templex.Templex]
     html_id: str = ""
     html_class: str = ""
