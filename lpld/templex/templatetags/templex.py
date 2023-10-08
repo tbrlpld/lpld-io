13d913a1d72a799c57146578689fc5468d929021
Tibor Leupold <tibor@lpld.io>
2023-10-07 17:03:22 -0700

    Render non-templex object as strings
    This is for interoperability

--- lpld/templex/templatetags/templex.py
+++ lpld/templex/templatetags/templex.py
@@ -1,11 +1,15 @@
 from django import template
 
+from lpld import templex as templex_module
+
 register = template.Library()
 
 
 @register.simple_tag(takes_context=False)
-def templex(templex_obj, **kwargs):
+def templex(obj, **kwargs):
     """Render a templex object."""
-    return templex_obj.render(**kwargs)
+    if isinstance(obj, templex_module.Templex):
+        return obj.render(**kwargs)
+    return str(obj)
 
 
