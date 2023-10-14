16e819e37eaf91082145ec17bbb1bcc0b523ece7
Tibor Leupold <tibor@lpld.io>
2023-10-14 16:03:09 -0700

    Add get_context_data method for flexibility

--- lpld/templex/templex.py
+++ lpld/templex/templex.py
@@ -21,6 +21,23 @@ class Templex(abc.ABC):
     def get_template(self) -> str:
         return self.template
 
+    def get_context_data(self) -> dict:
+        """
+        Return a dictionary of data to be passed to the template.
+
+        By default, this class attempts to convert the object itself into a dictionary.
+        """
+        if dataclasses.is_dataclass(self):
+            # Shallow copy to avoid the resolution of nested templexes. We need the
+            # nested templexes to stay templexes so that they keep their render method
+            # and instead of being turned into plain dicts.
+            return dict(
+                (field.name, getattr(self, field.name))
+                for field in dataclasses.fields(self)
+            )
+        else:
+            return self.__dict__.copy()
+
     def render(self, **kwargs: dict) -> "safestring.SafeString":
         """
         Render the templex by passing its data into the template.
@@ -28,16 +45,7 @@ class Templex(abc.ABC):
         The context can be extended by passing keyword arguments to this method.
 
         """
-        if dataclasses.is_dataclass(self):
-            # Shallow copy to avoid the resolution of nested templexes. We need the
-            # nested templexes to stay templexes so that they keep their render method
-            # and instead of being turned into plain dicts.
-            data_dict = dict(
-                (field.name, getattr(self, field.name))
-                for field in dataclasses.fields(self)
-            )
-        else:
-            data_dict = self.__dict__.copy()
+        data_dict = self.get_context_data()
         if kwargs:
             data_dict.update(kwargs)
         templex_template = loader.get_template(self.get_template())
